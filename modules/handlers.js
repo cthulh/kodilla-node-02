1764e97982914c4403745868eb5a472ea104eff8
Mariusz Borowiak <borowiakm85@gmail.com>
2018-06-19 16:57:37 +0100

    Add uploading and showing uploaded pic

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,30 +1,42 @@
 var fs = require('fs');
+var formidable = require('formidable');
 
 var params = {
   upload: {
-    log: 'Rozpoczynam obsługę żądania upload.',
-    write: 'Rozpoczynam upload!'
+    log: 'Processing upload request.',
+    write: 'Received image:<br/><img src="/show" />',
+    path: 'uploads/'
   },
   welcome: {
-    log: 'Rozpoczynam obsługę żądania welcome.',
+    log: 'Processing welcome request.',
     write: 'templates/start.html'
   },
   error: {
-    log: 'Nie wiem co robić.',
+    log: 'Request not recognised.',
     write: '404 :('
+  },
+  show: {
+    log: 'Processing uploaded picture...',
+    uploaded: ''
   }
 }
 
 function upload(request, response) {
     console.log(params.upload.log);
-    response.write(params.upload.write);
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+      fs.renameSync(files.upload.path, params.upload.path + files.upload.name);
+      params.show.uploaded = files.upload.name;
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(params.upload.write);
+      response.end();
+    });
 }
 
 function welcome(request, response) {
     console.log(params.welcome.log);
     fs.readFile(params.welcome.write, function(err, html) {
-      response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+      response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
       response.write(html);
       response.end();
     });
@@ -36,8 +48,18 @@ function error(request, response) {
     response.end();
 }
 
+function show(request, response) {
+  console.log(params.show.log);
+  fs.readFile(params.upload.path + params.show.uploaded, 'binary', function(error, file) {
+    response.writeHead(200, {'Content-Type': 'image/png'});
+    response.write(file, 'binary');
+    response.end();
+  });
+}
+
 module.exports = {
   upload: upload,
   welcome: welcome,
-  error: error
+  error: error,
+  show: show
 }
--- modules/server.js
+++ modules/server.js
@@ -27,6 +27,9 @@ function start() {
       case '/upload':
         handlers.upload(request, response);
         break;
+      case '/show':
+        handlers.show(request, response);
+        break;
       default:
         handlers.error(request, response);
     }
